17f0dc1a2a8f963ed2a1b05c51b8d983e76fce3b
tryonelove <tryonelove@protonmail.com>
2019-07-20 14:50:43 +0300

    User object

---
+++ user.py
@@ -0,0 +1,22 @@
+import api
+from config import API_KEY
+
+def formatAccuracy(num):
+    num = round(float(num), 2)
+    if num.is_integer():
+        return round(num)
+    return num
+
+class User(dict):
+    def __init__(self, uname, mode):
+        self.uname = uname
+        self.mode = mode
+        self.getUserStats()
+
+    def getUserStats(self):
+        API = api.BanchoApi(API_KEY)
+        self.user = API.get_user(u=self.uname, m=self.mode)[0]
+        self.user["accuracy"] = str(formatAccuracy(self.user["accuracy"]))
+
+    def __getattr__(self, attr):
+        return self.user.get(attr)
